72de280137dbc8f315c62a28fff9e1202e9227ee
Joe George <63401750+artisticwrist@users.noreply.github.com>
2022-05-28 20:53:57 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+str1 = input("string1: ")
+str2 = input("string2: ")
+sorted_str1 = sorted(str1)
+sorted_str2 = sorted(str2)
+
+if sorted_str1 == sorted_str2:
+    print("true")
+else:
+    print("false")
